5aa4116d3abc37f2904b58b402fcfe348fd87ae5
Abed001 <abed.alroufe@gmail.com>
2022-09-10 01:05:09 +0300

    finishing implemetationof gender api

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 window.onload = () => {
-    const url="https://dog.ceo/api/breeds/image/random";
-    const gender_url="https://api.genderize.io?name=peter";
-    const dog=document.getElementById("img");
-    const para = document.getElementById("para-sub");
-    const btndog = document.getElementById("button");
-    const btnsub = document.getElementById("button-sub");
-    const inputid = document.getElementById("InputId");
-  
-    //btndog.addEventListener('click',getRandomDog);
+const url="https://dog.ceo/api/breeds/image/random";
+const dog=document.getElementById("img");
+const para = document.getElementById("para-sub");
+const btndog = document.getElementById("button");
+const btnsub = document.getElementById("button-sub");
+let inputid = document.getElementById("inputId");
 
-    // btnsub.addEventListener('click',getGender);
+//btndog.addEventListener('click',getRandomDog);
+
 
     function getGender(){
-        fetch(gender_url)
-        .then(response => response.json())
-        .then(text =>{
-            para.innerHTML=`<p >${text.gender}</p>`
-        })
-    }
-      
-    function getRandomDog(){
-        fetch(url).
-        then(res=>res.json())
-        .then(data=>{
-            dog.innerHTML=`<img src="${data.message}"/>`
-        })
-    }
-   // getGender()
-    getRandomDog()
-    btnsub.addEventListener('click',getGender);
-    }
+        const textinput=inputid.value.trim();
+            const response= fetch( `https://api.genderize.io?name=${textinput}`).
+            then(response=>response.json())
+            .then(data=>{
+                para.innerHTML=`<p>${data.gender}</p>`;
+            })
+            
+            //console.log(data);
+            }
+    
+function getRandomDog(){
+    fetch(url).
+    then(res=>res.json())
+    .then(data=>{
+        dog.innerHTML=`<img src="${data.message}"/>`
+    })
+}
+// getGender()
+getRandomDog()
+btnsub.addEventListener('click',getGender);
 
-          
-   
+}
+        
 
-   
+
