3a075df25e5e50e2c83860f5db85735295bb74c2
Abed001 <abed.alroufe@gmail.com>
2022-09-10 14:43:37 +0300

    finishing the implementaion of nationality api

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.onload = () => {
 const url="https://dog.ceo/api/breeds/image/random";
 const dog=document.getElementById("img");
-const para = document.getElementById("para-sub");
+const paragender = document.getElementById("para-gender");
+const paraage = document.getElementById("para-age");
+const paranation = document.getElementById("para-nation");
 const btndog = document.getElementById("button");
 const btnsub = document.getElementById("button-sub");
 let inputid = document.getElementById("inputId");
@@ -14,11 +16,52 @@ let inputid = document.getElementById("inputId");
             const response= fetch( `https://api.genderize.io?name=${textinput}`).
             then(response=>response.json())
             .then(data=>{
-                para.innerHTML=`<p>${data.gender}</p>`;
+                paragender.innerHTML=`<p>${data.gender}</p>`;
             })
             
             //console.log(data);
             }
+            
+    function getAge(){
+        const textinput=inputid.value.trim();
+            const response= fetch( ` https://api.agify.io/?name=${textinput}`).
+            then(response=>response.json())
+            .then(data=>{
+                paraage.innerHTML=`<p>${data.age}</p>`;
+            })
+            
+            //console.log(data);
+            }
+
+           function getNationality(){
+                const textinput=inputid.value.trim();
+                    const response= fetch( `https://api.nationalize.io/?name=${textinput}`).
+                    then(response=>response.json())
+                    .then(data=>{
+                       paranation.innerHTML=`<p>${data.country[0].country_id +  data.country[1].country_id}</p>`;
+                    })
+            
+                    //console.log(data);
+                    }
+
+
+                    
+          /*  function getNationality(){
+                const textinput=inputid.value.trim();
+                    const response= fetch( `https://api.nationalize.io/?name=${textinput}`).
+                    then(response=>response.json())
+                    .then(data=>{
+                      // let breedsObject = data.country;
+                      let breedsobject=JSON.parse(data.country)[1].country_id;
+                       // breedsObject=JSON.stringify(breedsObject);
+                      //  const breedsArray = Object.keys(breedsObject);
+                       // for (let i = 0; i < 2; i++) {
+                          
+                      paranation.innerHTML=`<p>${breedsobject}</p>`;//}
+                    })
+                    
+                    //console.log(data);
+                    }*/
     
 function getRandomDog(){
     fetch(url).
@@ -30,6 +73,8 @@ function getRandomDog(){
 // getGender()
 getRandomDog()
 btnsub.addEventListener('click',getGender);
+btnsub.addEventListener('click',getAge);
+btnsub.addEventListener('click',getNationality);
 
 }
         
